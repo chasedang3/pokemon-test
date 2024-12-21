245d8ba1469056cbd957bbd4638f93414f8fbfbf
Nhue Dang <106152650+chasedang3@users.noreply.github.com>
2024-12-21 11:03:46 +0700

    add form layout

--- pokemon-frontend/src/app/dashboard/dashboard.component.ts
+++ pokemon-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 import { CarouselComponent } from "../carousel/carousel.component";
 import { MainContentComponent } from "../main-content/main-content.component";
+import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HeaderComponent, CarouselComponent, MainContentComponent, FooterComponent],
+  imports: [CarouselComponent, MainContentComponent, HeaderComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
